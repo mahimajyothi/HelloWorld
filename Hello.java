4f7df38942a3f92c8c3d7cc7193f2e9e17ff47cf
mahimajyothi <mahimajyothi1@gmail.com>
2020-09-04 10:47:13 +0530

    modified helloworld

--- Hello.java
+++ Hello.java
@@ -2,11 +2,11 @@ public class Hello{
 
        public static void main(String[] args){
 	   
-	        for(int i=1;i<=5;i++){
+	        
 			
-			   System.out.println("Hello World..."+i);
+			   System.out.println("Hello World... how are you");
 			   
-	        }
+	        
 	    
 		}
 	
